901a6c08ddb4341e7c7d54bec05859525804a404
Melissa <melissa.alasalmi@gmail.com>
2021-11-19 20:16:42 -0100

    3.4 done and working

--- index.js
+++ index.js
@@ -43,12 +43,15 @@ app.get('/api/info', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
 	const id = Number(request.params.id)
 	person = persons.find(person => person.id === id)
-	if (person) {
-		response.json(person)
-	} else {
-		response.status(404).end()
-	}
+	person ? response.json(person) : response.status(404).end()
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	persons = persons.filter(person => person.id !== id)
+  
+	response.status(204).end()
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
