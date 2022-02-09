fd9352debc1fff22abd5cdda764ce6002a46aa37
Melissa <melissa.alasalmi@gmail.com>
2022-02-09 10:01:54 +0200

    3.9 and 3.10 done

--- phonebook/frontend/src/components/AddContact.js
+++ phonebook/frontend/src/components/AddContact.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let baseUrl = 'http://localhost:3001/api/persons'
+let baseUrl = '/api/persons'
 
 const createContact = newContact => {
 	const request = axios.post(baseUrl, newContact)
--- phonebook/frontend/src/components/Contacts.js
+++ phonebook/frontend/src/components/Contacts.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 
-let baseUrl = 'http://localhost:3001/api/persons/'
+let baseUrl = '/api/persons/'
 
 const Contact = ({contact}) => <label>{contact} </label>
 
