b52502be03a76e4fb8f88e83e5ff847c840066eb
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2022-02-13 13:52:19 +0200

    -> 3.18 done

--- phonebook/index.js
+++ phonebook/index.js
@@ -16,9 +16,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :n
 app.use(cors());
 
 app.get('/api/persons', (request, response) => {
-  Contact.find({}).then(persons => {
-    response.json(persons)
-  })
+  Contact.find({}).then(persons => response.json(persons))
 })
 
 app.get('/api/info', (request, response) => {
@@ -26,28 +24,22 @@ app.get('/api/info', (request, response) => {
   Contact.find({}).then(persons => {
     response.send(
       `<p>Phonebook has info for ${persons.length} people</p>
-      <p>${date.toGMTString()}</p>`
-    )
+      <p>${date.toGMTString()}</p>`)
   })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
   Contact.findById(request.params.id)
     .then(contact => {
-      if (contact) {
-        response.json(contact)
-      } else {
-        response.status(404).end()
-      }
+      if (contact) response.json(contact) 
+      else response.status(404).end()
     })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   Contact.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
+    .then(() => response.status(204).end())
     .catch(error => next(error))
 })
 
@@ -55,24 +47,20 @@ app.post('/api/persons', (request, response) => {
   const { body } = request;
 
   if (!body.name) {
-    return result.status(400).json({
+    return response.status(400).json({
       error: 'name required',
     })
   }
   if (!body.number) {
-    return result.status(400).json({
+    return response.status(400).json({
       error: 'number required',
     })
   }
-
   const person = new Contact({
     name: body.name,
     number: body.number,
   });
-
-  person.save().then(savedContact => {
-    response.json(savedContact)
-  })
+  person.save().then(savedContact => response.json(savedContact))
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -81,24 +69,18 @@ app.put('/api/persons/:id', (request, response, next) => {
     name: body.name,
     number: body.number,
   }
-
   Contact.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedPerson => {
-      response.json(updatedPerson.toJSON())
-    })
-    .catch((error) => next(error))
+    .then(updatedPerson => response.json(updatedPerson))
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformed id' })
   } 
-
   next(error)
 }
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
